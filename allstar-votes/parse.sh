5b9dabb6d6aa0899df113da6ef673d6ef23a2de1
Oliver Schwartz <ollieschwartz99@gmail.com>
2019-03-07 15:49:56 -0500

    done correct parsing and updated players.txt

--- allstar-votes/parse.sh
+++ allstar-votes/parse.sh
@@ -1,7 +1,7 @@
 # Make 3 seperate csv files with (player_name, number of votes), one file for each season
 for i in 17 18 19; do
     for j in *$i.html; do
-        grep -o "html\">[A-Za-z]* [A-Za-z]*</a></td><td class=\"right \" data-stat=\"fan_votes\">.*fan_rank" $j > parsed
+        grep -o -E "html\">.{3,40}</a></td><td class=\"right \" data-stat=\"fan_votes\">.*fan_rank" $j > parsed
         sed "s/html\">//g" parsed > parsed1
         sed -r "s/<.*fan_votes\">/,/g" parsed1 > parsed2
         sed -r "s/<.*fan_rank//g" parsed2 > parsed3
