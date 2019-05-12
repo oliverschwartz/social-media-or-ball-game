57591f89f8795e7ca0e65043922c991e54011a38
Matt Nicholas <mn7@princeton.edu>
2019-05-12 12:43:13 -0400

    working models with Shapiro Test

--- kaggle/regressionModels.py
+++ kaggle/regressionModels.py
@@ -27,6 +27,8 @@ from pandas.plotting import scatter_matrix
 from sklearn import model_selection
 from sklearn.metrics import classification_report
 from yellowbrick.classifier import ClassificationReport
+from scipy.stats import shapiro
+from statsmodels.stats import stattools as stools
 
 
 
@@ -65,62 +67,126 @@ if year_holdout:
 	data_test = data_test[featuresUsed]
 
 	target_train = ABT[ABT['Year']!= year_holdout]
-	target_train = target_train['Votes']
+	target_train = target_train[['Votes']]
+
 
 	target_test = ABT[ABT['Year'] == year_holdout]
-	target_test = target_test['Votes']
+	target_test2 = target_test['Votes']
+	target_test = target_test[['Votes']]
+	
 
 
 
 
 
-def scoresRegression(model):
+def scoresRegression(y, model):
     
-    model.fit(data_train, target_train)
+    model.fit(data_train, target_train.values.ravel())
     pred = model.predict(data_test)
 
-    print("Mean squared error: %.3f" % mean_squared_error(pred, target_test))
-    print('R2 score: %.3f' % r2_score(pred, target_test))
+    print("Mean squared error: %.3f" % mean_squared_error(target_test, pred))
+    print('R2 score: %.3f' % r2_score(target_test, pred))
 
-    cvScore = cross_val_score(model, data_test, target_test, cv = 3, scoring = 'r2')
+    cvScore = cross_val_score(model, data_test, target_test.values.ravel(), cv = 3, scoring = 'r2')
+    #cvScore = cross_val_score(model, data_test, target_test, cv = 3, scoring = 'r2')
     print("R2 cross validation score: %0.2f (+/- %0.2f)" % (cvScore.mean(), cvScore.std() * 2))
 
-    #pred = [ '%.2f' % elem for elem in pred]
-    df = pd.DataFrame({'Actual': target_test, 'Predicted': pred})
+    #pred = [elem for elem in pred]
+    df = pd.DataFrame({'Actual': target_test2, 'Predicted': pred})
+    #print(df.head(10))
     df = df.sort_values('Predicted')
-    print(df.head())
     for item in df['Predicted']:
     	index = df[df.Predicted == item].index[0]
     	#print(index)
-    	#print(ABT.at[index, 'Player'])
+    	print(ABT.at[index, 'Player'])
     #print(df.head(20))
+    for i in pred:
+    	y.append(i)
 
 
 
 ##test1
+y_svr = []
 svr = SVR(kernel='rbf')
-print('\n\n\n SVR:\n')
-scoresRegression(svr)
+#print('\n\n\n SVR:\n')
+#scoresRegression(y_svr, svr)
 
+y_ridge = []
 print('\n\n RIDGE')
 reg = linear_model.Ridge(alpha=.5)
-scoresRegression(reg)
+scoresRegression(y_ridge, reg)
 
+y_Lasso = []
 print('\n\n LASSO')
 reg = linear_model.Lasso(alpha=.1)
-scoresRegression(reg)
+scoresRegression(y_Lasso, reg)
 
+y_EN = []
 print('\n\n Elastic Net')
 reg = ElasticNet(random_state = 0)
-scoresRegression(reg)
+scoresRegression(y_EN, reg)
 
+y_knn = []
 print('\n\n KNN')
 knn = neighbors.KNeighborsRegressor(n_neighbors = 7, weights = 'uniform')
-scoresRegression(knn)
+scoresRegression(y_knn, knn)
 
+y_rf = []
 print('\n\n RF')
 rf = RandomForestRegressor(random_state = 9, n_estimators = 100, criterion = 'mse')
-scoresRegression(rf)
+scoresRegression(y_rf, rf)
+#print(y_rf)
+
+def residuals(x, y):
+    #print(target_test.head())
+    resid = [i for i in (target_test['Votes'] - x)]
+    ssr = [i ** 2 for i in resid]
+    
+    ssrSum = 0
+    for i in ssr:
+        ssrSum += i
+        
+    standResid = []
+    for i in resid:
+        standResid.append(i / ((ssrSum / (target_test.shape[0] - 2)) ** (1/2)))
+    
+    for i in standResid:
+        y.append(i)
+
+
+#residuals
+#svrResid = []
+rfResid = []
+knnResid = []
+ridgeResid = []
+lassoResid =[]
+ENResid = []
+print('\n\n\n AQUIIIIII\n\n')
+#residuals(y_svr, svrResid)
+residuals(y_rf, rfResid)
+residuals(y_knn, knnResid)
+residuals(y_ridge, ridgeResid)
+residuals(y_Lasso, lassoResid)
+residuals(y_EN, ENResid)
+
+#shapiro wilkes
+#print(shapiro(svrResid))
+print(shapiro(rfResid))
+print(stools.durbin_watson(rfResid))
+print('\n\n')
+print(shapiro(knnResid))
+print(stools.durbin_watson(knnResid))
+print('\n\n')
+print(shapiro(ridgeResid))
+print(stools.durbin_watson(ridgeResid))
+print('\n\n')
+print(shapiro(lassoResid))
+print(stools.durbin_watson(lassoResid))
+print('\n\n')
+print(shapiro(ENResid))
+print(stools.durbin_watson(ENResid))
+
+#DW test
 
 
 
